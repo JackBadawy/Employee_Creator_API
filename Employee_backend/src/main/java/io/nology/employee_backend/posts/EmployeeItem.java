d8a69e8516e483f524d56093116dd179761c5403
Jack <jackbadawy1999@gmail.com>
2024-03-19 12:13:47 +1100

    fixed logging issue

--- Employee_backend/src/main/java/io/nology/employee_backend/posts/EmployeeItem.java
+++ Employee_backend/src/main/java/io/nology/employee_backend/posts/EmployeeItem.java
@@ -118,6 +118,14 @@ public class EmployeeItem {
 		this.currentEmployee = currentEmployee;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	
 
 
