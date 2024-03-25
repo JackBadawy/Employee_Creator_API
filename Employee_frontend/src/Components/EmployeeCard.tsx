b83d91b09053c1a9999f97e263006a834bf2a980
Jack <jackbadawy1999@gmail.com>
2024-03-25 12:06:13 +1100

    populated employee cards

--- Employee_frontend/src/Components/EmployeeCard.tsx
+++ Employee_frontend/src/Components/EmployeeCard.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const EmployeeCard = (employeeDetails) => {
   return (
-    <div>
-      <div>
+    <div className="employee-card">
+      <div className="employee-card__info">
         <h5>
           {employeeDetails.firstName} {employeeDetails.lastName}
         </h5>
+        <p>Contract -{employeeDetails.contractLength}</p>
+        <p>{employeeDetails.email}</p>
+      </div>
+      <div className="employee-card__buttons">
+        <button>Edit</button>
+        <button>Delete</button>
       </div>
     </div>
   );
