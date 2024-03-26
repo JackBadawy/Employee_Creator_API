2902cb1cb365aeefd8b6f07c1594f235b2af4d90
Jack <jackbadawy1999@gmail.com>
2024-03-26 20:22:04 +1100

    new employee page

---
+++ Employee_frontend/src/Pages/AddNewEmployeePage.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const AddNewEmployeePage = () => {
+  const [newEmployeeFormData, newEmployeeFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+  });
+
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="firstName"></label>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AddNewEmployeePage;
