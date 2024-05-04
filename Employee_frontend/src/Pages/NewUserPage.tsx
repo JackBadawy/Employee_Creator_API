d1d34b4e532d5aef52e905f3823b52bf81557e07
Jack <jackbadawy1999@gmail.com>
2024-05-05 08:54:37 +1000

    login page fully functional

--- Employee_frontend/src/Pages/NewUserPage.tsx
+++ Employee_frontend/src/Pages/NewUserPage.tsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import { addUser } from "../Services/User_services";
+import { useNavigate } from "react-router-dom";
+
 const NewUserPage = () => {
+  const [newUserFormData, setNewUserFormData] = useState({
+    username: "",
+    password: "",
+    approvedBy: null,
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewUserFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    setError("");
+    try {
+      const response = await addUser(newUserFormData);
+      console.log("User added:", response);
+
+      setNewUserFormData({ username: "", password: "" });
+
+      alert("User successfully added!");
+
+      returnHome();
+    } catch (error) {
+      setError("Failed to add user: " + error.message);
+      console.error("Error adding user:", error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const returnHome = () => {
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>New User Form</h1>
@@ -8,6 +56,26 @@ const NewUserPage = () => {
           to approve your user before you can access the repository
         </p>
       </aside>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={handleChange}
+        />
+        <label htmlFor="passsword">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+        />
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
+      </form>
     </div>
   );
 };
--- Employee_frontend/src/Services/User_services.ts
+++ Employee_frontend/src/Services/User_services.ts
@@ -6,3 +6,20 @@ export const getAllUsers = async () => {
   const data = await response.json();
   return data;
 };
+
+export const addUser = async (userData) => {
+  const response = await fetch("http://localhost:8080/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to add new user");
+  }
+
+  const data = await response.json();
+  return data;
+};
