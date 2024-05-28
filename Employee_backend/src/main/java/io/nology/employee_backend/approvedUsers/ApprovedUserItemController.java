1831532e272cb60e5dcaca32fe97229d217c389f
Jack <jackbadawy1999@gmail.com>
2024-05-28 19:22:15 +1000

    readme

--- Employee_backend/src/main/java/io/nology/employee_backend/approvedUsers/ApprovedUserItemController.java
+++ Employee_backend/src/main/java/io/nology/employee_backend/approvedUsers/ApprovedUserItemController.java
@@ -1,5 +1,7 @@
 package io.nology.employee_backend.approvedUsers;
 
+
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,70 +24,80 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/users")
 public class ApprovedUserItemController {
-	@Autowired
-	private ApprovedUserItemService approvedUserItemService;
-	
-	@PostMapping
-	public ResponseEntity<ApprovedUserItem> createPost(@Valid @RequestBody CreateApprovedUserDTO data) {
-		ApprovedUserItem createdItem = this.approvedUserItemService.createItem(data);
-		return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
-	}
-	
-	@GetMapping
-	public ResponseEntity<List<ApprovedUserItem>> getAllPosts() {
-		List<ApprovedUserItem> allItems = this.approvedUserItemService.getAll();
-		return new ResponseEntity<>(allItems, HttpStatus.OK);
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<ApprovedUserItem> getItemById(@PathVariable Long id) {
-		Optional<ApprovedUserItem> maybeItem = this.approvedUserItemService.findItemById(id);
-		
-		if (maybeItem.isPresent()) {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApprovedUserItemController.class);
+
+    @Autowired
+    private ApprovedUserItemService approvedUserItemService;
+
+    @PostMapping
+    public ResponseEntity<ApprovedUserItem> createPost(@Valid @RequestBody CreateApprovedUserDTO data) {
+        ApprovedUserItem createdItem = this.approvedUserItemService.createItem(data);
+        logger.info("Created ApprovedUserItem with ID: {}", createdItem.getId());
+        return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ApprovedUserItem>> getAllPosts() {
+        List<ApprovedUserItem> allItems = this.approvedUserItemService.getAll();
+        logger.info("Retrieved {} ApprovedUserItems", allItems.size());
+        return new ResponseEntity<>(allItems, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApprovedUserItem> getItemById(@PathVariable Long id) {
+        Optional<ApprovedUserItem> maybeItem = this.approvedUserItemService.findItemById(id);
+        if (maybeItem.isPresent()) {
             ApprovedUserItem foundItem = maybeItem.get();
+            logger.info("Retrieved ApprovedUserItem with ID: {}", id);
             return new ResponseEntity<>(foundItem, HttpStatus.OK);
         } else {
+            logger.warn("ApprovedUserItem with ID: {} not found", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-	}
-	
-	@PatchMapping("/{id}")
-	public ResponseEntity<ApprovedUserItem> updateItemById(@Valid @RequestBody UpdateApprovedUserDTO data, @PathVariable Long id) {
-		
-		Optional<ApprovedUserItem> maybeUpdatedItem = this.approvedUserItemService.updateById(data, id);
-		
-		if (maybeUpdatedItem.isPresent()) {
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApprovedUserItem> updateItemById(@Valid @RequestBody UpdateApprovedUserDTO data, @PathVariable Long id) {
+        Optional<ApprovedUserItem> maybeUpdatedItem = this.approvedUserItemService.updateById(data, id);
+        if (maybeUpdatedItem.isPresent()) {
             ApprovedUserItem foundItem = maybeUpdatedItem.get();
+            logger.info("Updated ApprovedUserItem with ID: {}", id);
             return new ResponseEntity<>(foundItem, HttpStatus.OK);
         } else {
+            logger.warn("ApprovedUserItem with ID: {} not found for update", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<ApprovedUserItem> deletePostById(@PathVariable Long id) {
-		boolean deleted = this.approvedUserItemService.deletePostById(id);
-		
-		if (!deleted) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-	}
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApprovedUserItem> deletePostById(@PathVariable Long id) {
+        boolean deleted = this.approvedUserItemService.deletePostById(id);
+        if (!deleted) {
+            logger.warn("ApprovedUserItem with ID: {} not found for deletion", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Deleted ApprovedUserItem with ID: {}", id);
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-    });
-    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        logger.error("Validation error: {}", errors);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
-}
+
--- README.md
+++ README.md
@@ -4,9 +4,4 @@
 
 this is a full stack application made with Java/Springboot on the backend and Typescript/React on the frontend
 the backend of this app has full crud functionality and other business logic, the React front end of
-this app allows users to sign in, create a new user (user must be approved before directory can be viewed) read the employee database in an intuitive way as well as fill out a form to
-create or edit employee info
-
-## Live Site
-
--TBD
+this app allows users to sign in, create a new user (user must be approved before directory can be viewed) read the employee database in an intuitive way as well as fill out a form to create or edit employee info
