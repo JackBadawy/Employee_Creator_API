ea6e88032bff818995153820c63d951330e0a770
Jack <jackbadawy1999@gmail.com>
2024-05-30 17:50:28 +1000

    header update

--- Employee_frontend/src/Services/User_services.ts
+++ Employee_frontend/src/Services/User_services.ts
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const getAllUsers = async () => {
   const response = await fetch(`${BASE_URL}/users`, {
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "text/json",
     },
   });
   if (!response.ok) {
